40f6f8986a5e387b19a34f06b01b634f30a99057
Ade Prianto <adeprianto21@upi.edu>
2020-08-05 17:27:48 +0700

    Menambahkan Login Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
+import Header from './components/Header/Header';
 import HomePage from './components/Pages/Home/Home';
 import LoginPage from './components/Pages/Login/Login';
 import RegisterPage from './components/Pages/Register/Register';
@@ -10,6 +11,7 @@ import NotFoundPage from './components/Pages/NotFound/NotFound';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route path='/user/register'>
           <RegisterPage />
--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Container, Jumbotron, Button } from 'react-bootstrap';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Container className='mt-5'>
+      <Jumbotron className='text-center'>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant='primary'>Learn more</Button>
+        </p>
+      </Jumbotron>
+    </Container>
+  );
 };
 
 export default Home;
