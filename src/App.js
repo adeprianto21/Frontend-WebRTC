f65369abe9512d1b1d75ce85b5a53beda97ef146
Ade Prianto <adeprianto21@upi.edu>
2020-08-05 15:31:47 +0700

    Menambahkan Beberapa Halaman

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
+import HomePage from './components/Pages/Home/Home';
+import LoginPage from './components/Pages/Login/Login';
+import RegisterPage from './components/Pages/Register/Register';
+import DashboardPage from './components/Pages/Dashboard/Dashboard';
+import NotFoundPage from './components/Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div>
-      <h1>Tes</h1>
+      <Switch>
+        <Route path='/user/register'>
+          <RegisterPage />
+        </Route>
+        <Route path='/user/login'>
+          <LoginPage />
+        </Route>
+        <Route path='/user/dashboard'>
+          <DashboardPage />
+        </Route>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
