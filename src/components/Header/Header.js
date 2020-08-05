0b77e93a07b41672c0e9777412e2170cbb672e2f
Ade Prianto <adeprianto21@upi.edu>
2020-08-05 20:27:19 +0700

    Update Navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,45 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const token = useSelector((state) => state.auth.token);
+  const role = useSelector((state) => state.auth.role);
+
+  let linkItems = (
+    <>
+      <Nav.Link as={Link} to='/admin/login'>
+        Admin Login
+      </Nav.Link>
+      <Nav.Link as={Link} to='/admin/register'>
+        Admin Register
+      </Nav.Link>
+      <Nav.Link as={Link} to='/user/login'>
+        User Login
+      </Nav.Link>
+      <Nav.Link as={Link} to='/user/register'>
+        User Register
+      </Nav.Link>
+    </>
+  );
+
+  if (token && role) {
+    if (role === 'Admin') {
+      linkItems = (
+        <Nav.Link as={Link} to='/admin/dashboard'>
+          Dashboard
+        </Nav.Link>
+      );
+    } else {
+      linkItems = (
+        <Nav.Link as={Link} to='/user/dashboard'>
+          Dashboard
+        </Nav.Link>
+      );
+    }
+  }
+
   return (
     <Navbar bg='primary' expand='lg' variant='dark'>
       <Container>
@@ -15,18 +52,7 @@ const Header = () => {
             <Nav.Link as={Link} to='/'>
               Home
             </Nav.Link>
-            <Nav.Link as={Link} to='/admin/login'>
-              Admin Login
-            </Nav.Link>
-            <Nav.Link as={Link} to='/admin/register'>
-              Admin Register
-            </Nav.Link>
-            <Nav.Link as={Link} to='/user/login'>
-              User Login
-            </Nav.Link>
-            <Nav.Link as={Link} to='/user/register'>
-              User Register
-            </Nav.Link>
+            {linkItems}
           </Nav>
         </Navbar.Collapse>
       </Container>
